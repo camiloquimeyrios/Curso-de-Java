da8d6f784dfc4a8ad8a6445d93d409dcc50abe58
Camilo Rios <camilaquimey71@gmail.com>
2022-12-29 19:34:42 -0300

    pre entrega

---
+++ js/main.js
@@ -0,0 +1,14 @@
+alert("Atención: Esta web está creada para la pre entrega de CoderHouse.");
+
+function calcularEdad(fecha) {
+    let hoy = new Date();
+    let cumpleanos = new Date(fecha);
+    let edad = hoy.getFullYear() - cumpleanos.getFullYear();
+    let m = hoy.getMonth() - cumpleanos.getMonth();
+
+    if (m < 0 || (m === 0 && hoy.getDate() < cumpleanos.getDate())) {
+        edad--;
+    }
+
+    return edad;
+}
